2949ad594dd80244504bafb82edb238f8c72dcc2
unknown <altamashbagwan1@gmail.com>
2021-09-27 19:31:04 +0530

    Refactored: Remove Welcome massage, Functionig of Employee Present or Not

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,7 +2,6 @@ package javapractice;
 
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
-		System.out.println("Welcome Employee Wage");
 		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
 		System.out.println(empCheck);
 		if(empCheck == 1)
