2dcb75c198de5cb8145e833760456339af1ea5d0
unknown <altamashbagwan1@gmail.com>
2021-09-27 18:36:46 +0530

    Refactored: Functioning of Employee Present or Not

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,7 +3,18 @@ package javapractice;
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome Employee Wage");
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		System.out.println(empCheck);
+		if(empCheck == 1)
+		{
+			System.out.println("emolyee is present");
 	
+		}
+		else
+		{
+			System.out.println("employee is not present");
+		}
 	}
 
 }
+	
