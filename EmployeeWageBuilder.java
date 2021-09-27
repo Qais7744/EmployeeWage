c9b8781ee8f2de13cf4c39a7e059e6a4f13ea50f
unknown <altamashbagwan1@gmail.com>
2021-09-27 21:18:25 +0530

    Refactored: Functioning of Calculate Daily Employee Wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,17 +2,20 @@ package javapractice;
 
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		System.out.println(empCheck);
-		if(empCheck == 1)
-		{
-			System.out.println("emolyee is present");
-	
-		}
+		// Constants
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		// Variables
+		int empHrs = 0;
+		int empWage = 0;
+		// Computation
+		int empCheck = (int) (Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
 		else
-		{
-			System.out.println("employee is not present");
-		}
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 
 }
